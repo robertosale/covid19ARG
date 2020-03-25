0e43c26b0f3afa88246b3dc390b43983cc65cc14
benjamontero <benjamontero@gmail.com>
2020-03-24 22:15:48 -0300

    first commit with the map and effect hover

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './argMap.svg';
 import './App.css';
+import MapaArgentina from './components/MapaArgentina'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CoronaVirus ARG</h1>
+       <MapaArgentina/>
       </header>
     </div>
   );
