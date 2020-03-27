2ef679f84b7adb8129d801fa92bdf1c5d3528656
Roberto Sale <roberto.m.sale@gmail.com>
2020-03-27 18:11:51 -0300

    add analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import BottomStats from './components/BottomStats'
 import pais from './svgs/pais'
+import ReactGA from 'react-ga';
 
 import ContainerProvincias from './components/ContainerProvincias';
 
@@ -9,6 +10,12 @@ class App extends Component {
   state = {
       
     }
+
+  initializeReactGA(){
+    ReactGA.initialize('UA-162056074-1');
+    ReactGA.pageview('/homepage');
+  }
+
   render() { 
     //console.log("pais:::::::::::::",pais)
     return (
