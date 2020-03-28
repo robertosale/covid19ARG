2ba90a1f7691d94f18fac74b2919493faa2729ea
benjamontero <benjamontero@gmail.com>
2020-03-27 23:47:32 -0300

    fixed bottomStats

--- src/components/BottomStats.js
+++ src/components/BottomStats.js
@@ -4,7 +4,7 @@ import logoMuertos from "./img/muertos.png";
 import logoRecuperados from "./img/recuperados.png";
 class BottomStats extends Component {
     render() {
-        const {total}=this.props;
+        const { total } = this.props;
         return (
 
             <div class="col-12 col-lg-6 container align-self-start">
@@ -15,34 +15,49 @@ class BottomStats extends Component {
 
                     <div className='card-stats col-lg-12 col-3 btn btn-secondary pr-2 pl-2 pb-0 pt-0'>
                         <div className="row">
-                            <img src={logoInfectados} className="col-6 col-sm-12"/>
-                            <p className="m-0 d-none d-sm-block">Infectados</p>
-                            <h5 className="col-6 col-sm-12">{total[1]}</h5>
-                        </div>
-                    </div>
-                    
-                    <div className='card-stats col-lg-12 col-3 btn btn-danger pr-2 pl-2 pb-0 pt-0'>
-                        <div className="row">
-                            <img src={logoMuertos} className="col-6 col-sm-12" />
-                            <p className="m-0 d-none d-sm-block">Fallecidos</p>
-                            <h5 className="col-6 col-sm-12"> {total[2]}</h5>
+                            <div className="col-6 col-sm-12">
+                                <img src={logoInfectados} />
+                            </div>
+                            <div className="col-6 col-sm-12 p-0 align-self-center">
+                                <p className="m-0 d-none d-sm-block">Infectados</p>
+                                <h5 >{total[1]}</h5>
+
+                            </div>
                         </div>
                     </div>
 
-                    
-                    
+                    <div className='card-stats col-lg-12 col-3 btn btn-danger pr-2 pl-2 pb-0 pt-0'>
+                        <div className="row">
+                            <div className="col-6 col-sm-12">
+                                <img src={logoMuertos} />
+                            </div>
+                            <div className="col-6 col-sm-12 p-0 align-self-center">
+                                <p className="m-0 d-none d-sm-block">Fallecidos</p>
+                                <h5>{total[2]}</h5>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
                     <div className='card-stats col-lg-12 col-3 btn btn-success pr-2 pl-2 pb-0 pt-0'>
                         <div className="row">
-                            <img src={logoRecuperados} className="col-6 col-sm-12" />
-                            <p className="m-0 d-none d-sm-block">Recuperados</p>
-                            <h5 className="col-6 col-sm-12">{total[3]}</h5>
+                            <div className="col-6 col-sm-12">
+                                <img src={logoRecuperados} />
+                            </div>
+                            <div className="col-6 col-sm-12 p-0 align-self-center">
+                                <p className="m-0 d-none d-sm-block">Recuperados</p>
+                                <h5 >{total[3]}</h5>
+                            </div>
+
                         </div>
 
                     </div>
 
                 </div>
 
-            </div>
+            </div >
         );
     }
 }
